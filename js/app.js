c6048498b05bb6771930eb43cafcc8a996e11340
SandipNITA2025 <sarkarsandip7910@gmail.com>
2024-02-29 18:45:21 +0530

    Project initialize

---
+++ js/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
